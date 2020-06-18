e0e940a021566ecd3a0ee385bc2e3483ac4e2ae3
ardamaz <yold0001@algonquinlive.com>
2020-06-18 09:13:38 -0400

    Initial commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox theCheckBox = findViewById(R.id.theCheckBox);
+        theCheckBox.setOnClickListener( v ->
+                Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
+
+        Switch theSwitch = findViewById(R.id.theSwitch);
+        theSwitch.setOnCheckedChangeListener(((buttonView, isChecked) -> {
+            Snackbar.make(theSwitch, "The switch is now", Snackbar.LENGTH_LONG).show();
+        }));
     }
 }
